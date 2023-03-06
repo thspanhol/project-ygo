b0d42565d3d51a953652a07f929b21639e2a4110
Thales Spanhol <thales_spanhol@hotmail.com>
2023-03-05 21:13:20 -0300

    Test: renderização do app corrigida nos testes

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-YGO Project
+# :small_yellow_triangle_down: Projeto YGO :small_yellow_triangle_down:
+
+O projeto foi desenvolvido com o intuito de utilizar a API do ygoprodeck (https://ygoprodeck.com/api-guide/) para fazer uma ferramenta de busca de cartas dentro de um arquétipo específico, o escolhido para a aplicação foi o Blackwing!
+
+### :small_yellow_triangle_down: Instalação
+
+Clone o repositório com o comando:
+```
+git clone https://github.com/thspanhol/project-ygo.git
+```
+
+Depois entre na pasta do projeto e instale as dependências com o comando:
+```
+npm install
+```
+
+Após isso, para abrir a aplicação basta usar o comando:
+```
+npm start
+```
+### :small_yellow_triangle_down: Como Utilizar
+
+Ao abrir a aplicação será renderizada uma tela de apresentação informando do que o projeto se trata junto com um botão SEARCH. Clicando nesse botão a página será direcionada para a tela de exibição dos cards, onde terá um input que pode ser usado para buscar cartas pelo nome e também 3 botões que filtram a lista para apenas as cartas do tipo monstro, mágica ou armadilha. Clicando em qualquer carta da lista, a página será redirecionada para uma terceira tela com os detalhes da carta clicada e um botão para voltar para a lista.
+
+## :small_yellow_triangle_down: Possiveis alterações
+
+* O arquétipo de cartas escolhido pode ser alterado para outro, para isso basta alterar a string presente na linha 18 do arquivo AppContext (project-ygo\src\context\AppContext.jsx)
+* Exemplos de possíveis substituições:
+```
+https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Dark%20Magician
+```
+```
+https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Blue-Eyes
+```
+
+## :small_yellow_triangle_down: Executando os testes
+
+Os testes da tela de apresentação podem ser feitos com o comando:
+```
+npm run test
+```
+
+## :small_yellow_triangle_down: Construído com
+
+* React - O framework usado para desenvolver os componentes JSX
+* CSS - Para estilização da página
+* YGO API - Usada para buscar e gerar as cartas
+* Router Dom - Para determinar as rotas da aplicação
+* Jest e Testing Library - Para desenvolver os testes
+* Vanilla-Tilt - Para o efeito hover nas cartas
+
+## :small_yellow_triangle_down: Autor
+
+* **Thales Spanhol** - [Perfil no GitHub](https://https://github.com/thspanhol)
+
+---
--- src/tests/home.test.js
+++ src/tests/home.test.js
@@ -1,159 +1,53 @@
-/* import { render, screen } from "@testing-library/react"
-import { BrowserRouter } from "react-router-dom"
-import { App } from "../App"
-import { Login } from "../routes/Login"
-import "@testing-library/jest-dom"
-import { AppProvider } from "../context/AppContext"
-
-describe("teste", () => {
-    it("testando", () => {
-        render(
-            <AppProvider>
-              <BrowserRouter>
-            <Login />
-            </BrowserRouter>  
-            </AppProvider>
-            
-        )
-
-        expect(screen.getByText("BlackWing")).toBeInTheDocument();
-    })
-})
-
- */
-
-/* import { render, screen } from "@testing-library/react"
-import { App } from "../App"
-import { Login } from "../routes/Login"
-import { Search } from "../routes/Search"
-import { Card } from "../routes/Card"
-import "@testing-library/jest-dom"
-import { AppProvider } from "../context/AppContext"
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Navigate } from "react-router-dom";
-
-
-const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        { path: "/", element: <Login /> },
-        { path: "/search", element: <Search /> },
-        { path: "/card", element: <Card /> },
-        { path: "*", element: <Navigate to='/' /> },
-      ],
-    },
-  ]);
-
-describe("teste", () => {
-    it("testando", () => {
-        render(
-            <App />
-        )
-
-        expect(screen.getByText("BlackWing")).toBeInTheDocument();
-    })
-}) */
-
-/* import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
-import { Outlet, Route, Router, Routes, Navigate } from 'react-router-dom';
-
-const Layout = () => {
-    return (
-        <div className="layout">
-            <Outlet />
-        </div>
-    );
-};
-
-const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route element={<Layout />}>
-                <Route path="/sign-in" element={<p data-testid="login-wrapper" />} />
-            </Route>
-
-            <Route index element={<Navigate to="/sign-in" />} />
-        </Routes>
-    );
-};
-
-describe('Routes', () => {
-    it('Redirects to Login if not logged in', async () => {
-        const history = createMemoryHistory();
-        expect(history.location.pathname).toEqual('/');
-
-        // history.push('/sign-in');
-        render(
-            <Router location={history.location} navigator={history}>
-                <AppRoutes />
-            </Router>
-        );
-        expect(history.location.pathname).toEqual('/sign-in');
-
-        await waitFor(() => {
-            screen.getByTestId('login-wrapper');
-        });
-    });
-}); */
-
-/* import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import * as React from "react";
 import { render, waitFor, screen } from "@testing-library/react";
-
-import { AppProvider } from "../context/AppContext"
-
-import routesConfig from "../routes/Routes";
-
-
-describe("teste", () => {
-  it("testando", async () => {
-    const router = createMemoryRouter(routesConfig, { initialEntries: ["/"], });
-      render(
-        <AppProvider>
-          <RouterProvider router={router} />
-        </AppProvider>
-      )
-
-      expect(screen.getByText("BlackWing")).toBeInTheDocument();
-  })
-}); */
-
-import { RouterProvider, MemoryRouter, Routes, Route } from "react-router-dom";
-import { render, waitFor, screen } from "@testing-library/react";
-
-import { AppProvider } from "../context/AppContext"
-
-import routesConfig from "../routes/Routes";
-
-import { App } from "../App"
-import { Login } from "../routes/Login"
-import { Search } from "../routes/Search"
-import { Card } from "../routes/Card"
-
+import "@testing-library/jest-dom";
+import App from "../App"
+import Login  from "../routes/Login"
+import Search from "../routes/Search"
+import Card from "../routes/Card"
 import { Navigate } from "react-router-dom";
+import { AppProvider } from '../context/AppContext';
 
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Login /> },
+      { path: "/search", element: <Search /> },
+      { path: "/card", element: <Card /> },
+      { path: "*", element: <Navigate to='/' /> },
+    ],
+  },
+];
 
-describe("teste", () => {
-  it("testando", async () => {
-      render(
-        <AppProvider>
-          <MemoryRouter initialEntries={['/']}>
-            <Routes>
-              <Route element={<App />}>
-                <Route path="/" element={<Login />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/card" element={<Card />} />
-                <Route path="*" element={<Navigate to='/' />} />
-             </Route>
-            </Routes>
-          </MemoryRouter>
-        </AppProvider>
-      )
+const router = createMemoryRouter(routes, { initialEntries: ["/"] });
 
-      expect(screen.getByText("BlackWing")).toBeInTheDocument();
-  })
+const renderApp = <AppProvider><RouterProvider router={router} /></AppProvider>;
+
+describe("Testes da tela Home", () => {
+  test("Verifica se a tela contém todos os títulos", async () => {
+    render(renderApp);
+    
+    await waitFor(() => screen.findAllByRole("heading"));
+    expect(screen.getByText("BlackWing")).toBeInTheDocument();
+    expect(screen.getByText("Database")).toBeInTheDocument();
+    expect(screen.getByText("Searching for all cards of the")).toBeInTheDocument();
+    expect(screen.getByText("blackwing archetype.")).toBeInTheDocument();
+  });
+
+  test("Verifica se a tela contém o link para a página search", async () => {
+    render(renderApp);
+    
+    await waitFor(() => screen.findAllByRole("heading"));
+    expect(screen.getByText("Search")).toBeInTheDocument();
+  });
+
+  test("Verifica se a tela renderiza a imagem", async () => {
+    render(renderApp);
+    
+    await waitFor(() => screen.findAllByRole("heading"));
+    expect(screen.getByRole('img')).toBeInTheDocument();
+  });
 });
