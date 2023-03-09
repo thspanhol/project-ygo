71ab64ee45bc765d3928ca054347ff26169f71a6
Thales Spanhol <thales_spanhol@hotmail.com>
2023-03-09 20:59:17 -0300

    Feat: Testando correção das rotas no github pages

--- src/index.js
+++ src/index.js
@@ -5,10 +5,23 @@ import Login from './routes/Login';
 import Search from './routes/Search';
 import Card from './routes/Card';
 
-import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Navigate, createHashRouter } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Login /> },
+      { path: "/search", element: <Search mockCardList="" /> },
+      { path: "/card", element: <Card /> },
+      { path: "*", element: <Navigate to='/' /> },
+    ],
+  },
+]);
+
+const routerHash = createHashRouter([
   {
     path: "/",
     element: <App />,
@@ -25,7 +38,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <RouterProvider router={router} />
+      <RouterProvider router={routerHash} />
     </AppProvider>
   </React.StrictMode>
 );
